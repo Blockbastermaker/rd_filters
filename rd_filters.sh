7bbcaf6aafa265c54f15761c7363612a5004c939
Francois Berenger <unixjunkie@sdf.org>
2020-09-10 11:45:20 +0900

    new file:   rd_filters.sh
    shell wrapper to soft link in your ~/bin/

---
+++ rd_filters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x # DEBUG
+set -u
+
+HERE=~/src/rd_filters # your install setup
+
+${HERE}/rd_filters/rd_filters.py \
+    --rules ${HERE}/rules.json \
+    --alerts ${HERE}/alerts.csv "$@" # remaining CLI params
